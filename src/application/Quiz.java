f1568ebd6888ae2187940c6c7b7b30f64b6dd62d
Zhiyi Zhang <zzhang867@wisc.edu>
2019-04-24 18:29:39 -0500

    Implement Quiz class

--- src/application/Quiz.java
+++ src/application/Quiz.java
@@ -6,21 +6,22 @@ public class Quiz {
   double score = quizQuestion.length;
 
   Quiz (Question[] questions){
-    
+    this.quizQuestion = questions;
+    this.score = questions.length;
   }
   
   public Question[] getQuizQuestion() {
-    return null;
+    return quizQuestion;
   }
   
   // Set score 1 point off.
   public void pointDeduction() {
-    
+    score--;
   }
   
   // Convert score to percentage and return
   public double getScore() {
-    return -1;
+    return (score/quizQuestion.length)*100;
   }
   
 }
