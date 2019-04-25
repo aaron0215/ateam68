a454dcc60846a62bbc7f31019298f97fbd0ca342
Zhiyi Zhang <zzhang867@wisc.edu>
2019-04-25 18:39:13 -0500

    move to src folder

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,48 +1,35 @@
 package application;
-	
-import javafx.application.Application; 
-import javafx.scene.Group; 
-import javafx.scene.Scene; 
-import javafx.scene.shape.*; 
-import javafx.stage.Stage;  
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.geometry.Insets;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
-import javafx.event.ActionEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.geometry.Pos;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import java.awt.TextField;
-import java.util.*;
-
-
+import javafx.stage.Stage;
+/*from w  w  w.j ava  2s .c om*/
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+  @Override
+  public void start(Stage stage) {
+    HBox root = new HBox();
+    Scene scene = new Scene(root, 300, 150);
+    stage.setScene(scene);
+    stage.setTitle("");
+
+    ToggleGroup group = new ToggleGroup();
+    RadioButton button1 = new RadioButton("select first");
+    button1.setToggleGroup(group);
+    button1.setSelected(true);
+    RadioButton button2 = new RadioButton("select second");
+    button2.setToggleGroup(group);
+    
+    root.getChildren().add(button1);
+    root.getChildren().add(button2);
+
+    scene.setRoot(root);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
